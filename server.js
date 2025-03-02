361194af5795acd81c48b44343aa45d96c5fb70b
Luisirk <luisirk.montoto@gmail.com>
2025-03-02 18:36:23 +0100

    conect on server.js

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const taskRoutes = require("./routes/taskRoutes"); // Importar rutas
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("🔥 Conectado a MongoDB Atlas"))
+  .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
+
+// Usar las rutas de tareas
+app.use("/api", taskRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Conectar a MongoDB Atlas
-const uri = process.env.MONGO_URI; // La URL estará en el archivo .env
-mongoose.connect(process.env.MONGO_URI);
-
-
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send("🚀 Servidor funcionando correctamente");
-});
-
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
